f529d54c5f9d3d054c916e11c2b201660917fbf5
Banjos <brunosilveiradosanjos@gmail.com>
2025-02-28 22:58:27 -0300

    feat(lint): unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const unusedImports = require("eslint-plugin-unused-imports");
 
 module.exports = tseslint.config(
   {
@@ -13,6 +14,9 @@ module.exports = tseslint.config(
       ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
+    plugins: {
+      "unused-imports": unusedImports,
+    },
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
@@ -30,6 +34,17 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          "vars": "all",
+          "varsIgnorePattern": "^_",
+          "args": "after-used",
+          "argsIgnorePattern": "^_",
+        },
+      ]
     },
   },
   {
